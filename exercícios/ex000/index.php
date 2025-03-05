6ea1dfe8c6e83e961f5c1c32cdf4f95a5befd052
caio-sdm <marinhocaio1308@gmail.com>
2025-03-05 01:41:30 -0300

    primeiros ex de php 😄

---
+++ exercícios/ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php primeiro código</title>
+</head>
+<body>
+    <h1>
+    <?php 
+        echo "Olá mundo\u{1f30e} ";
+        print "Adeus mundo\u{1F525} ";
+    ?>
+    </h1>
+    <p>Que lindio ,,>u<,,</p>
+</body>
+</html>
