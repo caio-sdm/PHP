4da9709882e49c4f40498abef9e623e267821a89
caio-sdm <marinhocaio1308@gmail.com>
2025-03-10 21:41:21 -0300

    ex002

---
+++ exercícios/ex001.1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variavel</title>
+</head>
+<body>
+    <h1>Seu nome aparecerá abaixo :D</h1>
+    <?php
+        $nome = "caio"
+    ?>
+    <?= "<p> Seu nome é <strong>$nome</strong>! </p>" ?>
+
+</body>
+</html>
