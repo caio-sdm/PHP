3f665a216f2bbe58f7f882b6869fda6e270beb25
caio-sdm <marinhocaio1308@gmail.com>
2025-03-25 11:46:28 -0300

    ex003

---
+++ exercícios/ex003/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tipos primitivos php</title>
+</head>
+<body>
+    <h1>teste de tipos primitivos</h1>
+    <?php
+        //0x = hexadecimal; 0b = binário; 0 = octal 
+        // $num = 300;
+        // echo "o valor da variável é $num"; 
+        // $num2 = 0x1A;
+        // echo "o valor hexadecimal é $num2";
+        //$num3 = 3e2;
+        // echo "o valor de 3 * 10^2 é $num3";
+
+        // $v = 300;
+        // var_dump($v);
+
+        // $t = true;
+        // var_dump($t);
+
+        // $s = "caio";
+        // var_dump($s);
+        
+        // $st = (int) "950"; //seria string
+        // var_dump($st);
+
+        //$casado = false;
+        //print "O valor para casado é $casado";
+    
+        //array
+        // $vet = [6, 2, 7, 9, 5.5, "caio", true];
+        // var_dump($vet);
+    
+        //object
+        // class pessoa{
+        //     private string $nome;
+        //     private int $idade;
+        // }
+
+        // $p = new pessoa;
+        // var_dump($p);
+    ?>
+</body>
+</html>
